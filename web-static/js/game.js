87f490a35e61033faf10d5f8a24f001eaccdffb2
Mfeliciano778 <1652@holbertonschool.com>
2021-06-12 22:23:07 +0000

    Adding the information to the html

--- web-static/js/game.js
+++ web-static/js/game.js
@@ -1,8 +1,47 @@
-window.addEventListener('load', () => {
+$(document).ready( function () {
     const email = localStorage.getItem('EMAIL');
-    console.log(email);
+    // console.log(email);
     $.getJSON("http://localhost:5000/api/v1/users/email/" + email, function (data){
         if (data) {
+            userEmail = data['email'];
+            // console.log(userEmail)
+            username = data['username'];
+            // console.log(username);
+            resource = data['resource'];
+            // console.log(resource);
+            speed = data['drill_speed'];
+            // console.log(speed);
+            size = data['drill_size'];
+            // console.log(size);
+
+            document.getElementById("username").innerHTML = username;
+            document.getElementById("resource").innerHTML = "Resources: " + resource;
+            document.getElementById("speed").innerHTML = "Speed: " + speed;
+            document.getElementById("size").innerHTML = "Size: " + size;
+            localStorage.setItem("RESOURCE", resource);
+            localStorage.setItem("USERID", data['id']);
         }
     });
+    const planetBtn = document.querySelector('.planet');
+
+    planetBtn.addEventListener('click', () => {
+        let resources = localStorage.getItem('RESOURCE');
+        resources = parseInt(resources) + 1
+        const id = localStorage.getItem('USERID');
+        if (resource != null) {
+            $.ajax({
+                url: 'http://localhost:5000/api/v1/users/' + id,
+                type: 'PUT',
+                data: JSON.stringify({"resource": parseInt(resources)}),
+                dataType: 'json',
+                contentType: "application/json",
+                success: function(data, textStatus) {
+                    console.log(textStatus);
+                    console.log(data);
+                }
+            });
+            localStorage.setItem("RESOURCE", resources);
+            document.getElementById("resource").innerHTML = "Resources: " + resources;
+        }
+    })
 })
