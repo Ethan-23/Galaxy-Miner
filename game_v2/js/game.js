7a1abba21415189db266cfaf9f6d27ac19a32126
Mfeliciano778 <1652@holbertonschool.com>
2021-06-14 19:42:29 +0000

    Made our mvp seperate from version 2

---
+++ game_v2/js/game.js
@@ -0,0 +1,107 @@
+$(document).ready( function () {
+    const email = localStorage.getItem('EMAIL');
+    const planetBtn = document.querySelector('.planet');
+    const drill_speed = document.querySelector('.Uspeed');
+    const drill_size = document.querySelector('.Usize');
+    let resource_counter = 0;
+    let user_speed = 0;
+    let user_size = 0;
+    let user_id = 0;
+    let num = 0;
+
+    //get the User info with Email we got from login
+    $.getJSON("http://localhost:5000/api/v1/users/email/" + email, function (data){
+        if (data) {
+            userEmail = data['email'];
+            // console.log(userEmail)
+            username = data['username'];
+            // console.log(username);
+            resource = data['resource'];
+            // console.log(resource);
+            speed = data['drill_speed'];
+            // console.log(speed);
+            size = data['drill_size'];
+            // console.log(size);
+
+            //set elements to the data we get
+            document.getElementById("username").innerHTML = username;
+            document.getElementById("resource").innerHTML = "Resources: " + resource;
+            document.getElementById("speed").innerHTML = "Speed: " + speed;
+            document.getElementById("size").innerHTML = "Size: " + size;
+    
+            //storing the data locally to be abled to use it from anywhere
+            resource_counter = resource;
+            user_speed = speed;
+            user_size = size;
+            user_id = data['id'];
+            dataload();
+        }
+    });
+
+
+    //update Resources by 1
+    function udpateResource(amount) {
+        //getting the resources and updating it by 1
+        resource_counter += amount;
+        console.log(resource_counter);
+        //getting the id
+
+        //As long as resource exists...
+        if (resource != null) {
+            //udpate resources by 1
+            $.ajax({
+                url: 'http://localhost:5000/api/v1/users/' + user_id,
+                type: 'PUT',
+                data: JSON.stringify({"resource": resource_counter, "drill_speed": user_speed, "drill_size": user_size}),
+                dataType: 'json',
+                contentType: "application/json",
+                success: function(data, textStatus) {
+                    console.log(textStatus);
+                    // console.log(data);
+                }
+            });
+            //update the resource shown on the website
+            document.getElementById("resource").innerHTML = "Resources: " + resource_counter.toFixed(1);
+            document.getElementById("speed").innerHTML = "Speed: " + user_speed;
+            document.getElementById("size").innerHTML = "Size: " + user_size;
+        }
+    };
+
+
+    
+    drill_size.addEventListener('click',  function init() {
+        cost = 1 //cost of upgrade 
+        if(resource_counter >= cost){
+            resource_counter -= cost; 
+            user_size += 1;
+            udpateResource(0);
+        }
+    })
+
+
+
+    planetBtn.addEventListener('click', () => {
+        num = user_size + 1;
+        udpateResource(num);
+    })
+
+
+
+    drill_speed.addEventListener('click',  function init() {
+        cost = 1 //cost of upgrade 
+        if(resource_counter >= cost){
+            resource_counter -= cost; 
+            user_speed += 1;
+            udpateResource(0);
+        }
+    })
+    function dataload(){
+        if(user_speed > 0){
+            var int = self.setInterval(function () {
+                num = 1 * user_speed
+                udpateResource(num);
+                console.log("test");
+            }, 1000);
+        }
+    }
+})
---
+++ game_v2/js/login.js
@@ -0,0 +1,97 @@
+//getting all buttons and form window we are using in login
+const loginmodal = document.getElementById('login-modal');
+const loginopenBtn = document.querySelector('.loginbutton');
+const logincloseBtn = document.querySelector('.login-close-btn');
+
+
+//if there's a click on the login we open the form
+loginopenBtn.addEventListener('click', () => {
+    loginmodal.style.display = 'block';
+})
+
+
+//if there's a click close the form
+logincloseBtn.addEventListener('click', () => {
+    loginmodal.style.display = 'none';
+})
+
+
+//or if there's click outside the form (close the form)
+window.addEventListener('click', (e) => {
+    if(e.target === loginmodal){
+	loginmodal.style.display = 'none';
+    }
+})
+
+
+//getting the actual form inputs
+const logform = document.getElementById('logform');
+const logemail = document.getElementById('log-email');
+const logpassword = document.getElementById('log-password');
+
+
+//if there's an incorrect input show this message
+function showError(input, message) {
+    const formValidation = input.parentElement;
+    formValidation.className = 'login-form-val error';
+
+    const errorMessage = formValidation.querySelector('p');
+    errorMessage.innerText = message;
+}
+
+//if the inputs are correct show the green outline
+function showValid(input){
+    const formValidation = input.parentElement;
+    formValidation.className = 'login-form-val valid';
+}
+
+//make sure there is an input
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+	if(input.value.trim() === ''){
+	    showError(input, `${getFieldName(input)} is required`);
+	}else{
+	    showValid(input);
+	}
+    })
+}
+
+//get the input field name
+function getFieldName(input){
+    return input.name.charAt(0).toUpperCase() + input.name.slice(1);
+}
+
+//set email to store locally
+function storeSession(){
+    const fullemail = logemail.value;
+    localStorage.setItem("EMAIL", fullemail);
+    return;
+}
+
+//check if the user's email and password are correct
+function checkEmail(input, pass){
+    $.getJSON("http://localhost:5000/api/v1/users/email/" + input.value, function (data){
+        if (data) {
+            if (pass.value == data["password"]){
+                location.href = "../game_v2/main_page.html";
+                return;
+            }else {
+                showError(pass, 'incorrect password');
+                return;
+            }
+        }else {
+            showError(input, 'couldn\'t find your account');
+            showError(pass, "");
+            return;
+        }
+    });
+}
+
+//when submitted check all inputs and make sure they are correct
+logform.addEventListener('submit', (f) => {
+    f.preventDefault();
+    checkRequired([logemail, logpassword]);
+    checkEmail(logemail, logpassword);
+    storeSession();
+    return;
+})
